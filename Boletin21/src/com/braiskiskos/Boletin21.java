65366bfc3615104fca81beb17da0dc973ea65ab0
bmartinezparedes <72441274+bmartinezparedes@users.noreply.github.com>
2021-04-21 13:43:38 +0200

    Subo Boletin21
    Aquí va el boletín 21 totalmente echo

---
+++ Boletin21/src/com/braiskiskos/Boletin21.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.braiskiskos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Boletin21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Ventana1 v1=new Ventana1();
+        //v1.setVisible(true);
+
+        Ventana2 v2=new Ventana2();
+        v2.setVisible(true);
+    }
+    
+}
