abaf837063775f038ab64f344da6929ded230c78
bmartinezparedes <72441274+bmartinezparedes@users.noreply.github.com>
2021-04-22 13:49:08 +0200

    Boletin22
    Boletin 22 al completo

---
+++ Boletin22/src/com/braiskiskos/Boletin22.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.braiskiskos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Boletin22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Ventana v=new Ventana();
+        v.setVisible(true);
+    }
+    
+}
