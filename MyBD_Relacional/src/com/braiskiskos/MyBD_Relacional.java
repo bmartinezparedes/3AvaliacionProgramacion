33282798e34951cc59ecdf138555383589e9bbaf
bmartinezparedes <72441274+bmartinezparedes@users.noreply.github.com>
2021-05-17 18:22:56 +0200

    Base de datos

---
+++ MyBD_Relacional/src/com/braiskiskos/MyBD_Relacional.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.braiskiskos;
+
+import Ventana.VPrograma;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MyBD_Relacional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        VPrograma p=new VPrograma();
+        p.setVisible(true);
+    }
+    
+}
